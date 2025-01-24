7c3ee9f46ffdc4322b3b7f93b2b447cea17c2df4
KelvinFamilia <kelvinjosefamilia@gmail.com>
2025-01-24 16:04:58 +0000

    lab ok

--- js/script.js
+++ js/script.js
@@ -1,12 +1,60 @@
+// js/script.js
+
 window.onload = function () {
   const startButton = document.getElementById("start-button");
   const restartButton = document.getElementById("restart-button");
+  let game; // added
 
   startButton.addEventListener("click", function () {
     startGame();
   });
+  restartButton.addEventListener("click", ()=> {
+    GameRefresh();
+  })
 
   function startGame() {
     console.log("start game");
+    game = new Game(); // added
+
+    game.start(); // added
   }
+   // Function that handles keydown event
+   function handleKeydown(event) {
+    const key = event.key;
+    const possibleKeystrokes = [
+      "ArrowLeft",
+      "ArrowUp",
+      "ArrowRight",
+      "ArrowDown",
+    ];
+
+    // Check if the pressed key is in the possibleKeystrokes array
+    if (possibleKeystrokes.includes(key)) {
+      event.preventDefault();
+
+      // Update player's directionX and directionY based on the key pressed
+      switch (key) {
+        case "ArrowLeft":
+          game.player.directionX = -1;
+          break;
+        case "ArrowUp":
+          game.player.directionY = -1;
+          break;
+        case "ArrowRight":
+          game.player.directionX = 1;
+          break;
+        case "ArrowDown":
+          game.player.directionY = 1;
+          break;
+      }
+    }
+  }
+
+  function GameRefresh (){
+    window.location.reload()
+  }
+
+  // Add the handleKeydown function as an event listener for the keydown event
+  window.addEventListener("keydown", handleKeydown);
+
 };
